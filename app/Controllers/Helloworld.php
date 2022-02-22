ce6b462a3739c7642dacdc0a06471199b3bf8e4d
Danilo Henrique <daniloohenriquee@gmail.com>
2022-02-21 21:35:44 -0300

    Agora sim, First with Codeigniter4. Hello World de Respeito :)

---
+++ app/Controllers/Helloworld.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Helloworld extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Hello World, CodeIgniter4</h1>";
+    }
+}
