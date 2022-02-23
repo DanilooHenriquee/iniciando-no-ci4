04d562fea79f3dc152eeaa25ce0c26938874c4b9
Danilo Henrique <daniloohenriquee@gmail.com>
2022-02-22 22:02:55 -0300

    adicionando novo controler, helloworld

--- app/Controllers/Helloworld.php
+++ app/Controllers/Helloworld.php
@@ -4,10 +4,20 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 
-class Helloworld extends BaseController
-{
-    public function index()
-    {
+class Helloworld extends BaseController {
+    public function index() {
         echo "<h1>Hello World, CodeIgniter4</h1>";
+    }
+
+    public function nome($nome, $idade, $sexo = null, $peso = null) {
+        echo "<h1> Me fale sobre você ! </h1>";
+        echo "<h2> Meu nome é ${nome}, tenho ${idade} anos";
+        if ($sexo != null) {
+            echo ", sou do sexo ${sexo}";
+        }
+        if ($peso != null) {
+            echo " e meu peso atual é ${peso} kg";
+        }
+        echo ".</h2>";
     }
 }
