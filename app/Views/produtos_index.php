58a4e1091d3e9ae8ef173f171ef1d4c3e074a8e6
Danilo Henrique <daniloohenriquee@gmail.com>
2022-03-01 17:25:56 -0300

    Testando git

--- app/Views/produtos_index.php
+++ app/Views/produtos_index.php
@@ -3,3 +3,6 @@
     <br>
     <h2> <?php echo 'Produto: ' . $nome ?> </h2>
     <h2> <?php echo 'Preço: ' . $preco ?> </h2>
+
+    <h3>Testando a função do git</h3>
+    <h4>Testando a função do git</h4>
