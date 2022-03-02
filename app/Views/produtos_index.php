5c8ac6c010c335d9c3bc03d2d0b53cbea7cffca4
Danilo Henrique <daniloohenriquee@gmail.com>
2022-03-01 21:26:31 -0300

    ok

--- app/Views/produtos_index.php
+++ app/Views/produtos_index.php
@@ -3,6 +3,3 @@
     <br>
     <h2> <?php echo 'Produto: ' . $nome ?> </h2>
     <h2> <?php echo 'Preço: ' . $preco ?> </h2>
-
-    <h3>Testando a função do git</h3>
-    <h4>Testando a função do git</h4>
